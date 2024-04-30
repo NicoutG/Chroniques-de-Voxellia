3d58f469e5753e8529c08c439dd58ad98c8207ea
NicoutG <nicogallet01@gmail.com>
2024-04-30 18:40:38 +0200

    bloc chutant dans eau

--- src/BlocMortel.java
+++ src/BlocMortel.java
@@ -8,4 +8,12 @@ public class BlocMortel extends Bloc {
     public boolean deplacer (Bloc [][][] terrain, BlocType [] blocs, int x, int y, int z, int depx, int depy, int depz, Joueur joueur) {
         return true;
     }
+
+    @Override
+    public void miseAjour (Bloc [][][] terrain, BlocType [] blocs, int x, int y, int z, Joueur joueur) {
+        super.miseAjour(terrain, blocs, x, y, z, joueur);
+        if (z+1<terrain[x][y].length)
+            if (terrain[x][y][z+1] instanceof BlocMouvant)
+                terrain[x][y][z]=null;
+    }
 }
