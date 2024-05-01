a50803300811546d41a7a6d9b0bb8bac5bad62a2
maxime-antoine <maxime.antoine001@gmail.com>
2024-05-01 21:58:44 +0200

    animation,texture, and level8

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Niveau niveau=new Niveau();
         niveau.chargerBlocs("./data/texte/blocs.txt");
-        niveau.lancementNiveau("./data/texte/terrainTest7.txt");
+        niveau.lancementNiveau("./data/texte/terrainTest8.txt");
         MF mf=new MF(niveau);
         niveau.addObserver(mf);
         mf.setVisible(true);
