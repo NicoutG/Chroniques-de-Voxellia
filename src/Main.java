f0b1e7d3cf3f9b0cfe93d070165b5df79902429c
NicoutG <nicogallet01@gmail.com>
2024-05-05 14:06:30 +0200

    +3 blocs

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         Niveau niveau=new Niveau();
-        niveau.chargerBlocs("./data/texte/blocs.txt");
-        niveau.lancementNiveau("./data/texte/terrainTest10.txt");
+        niveau.chargerBlocs("./data/texte/blocs/blocs.txt");
+        niveau.lancementNiveau("./data/texte/niveaux/terrainTest10.txt");
         MF mf=new MF(niveau);
         niveau.addObserver(mf);
         mf.setVisible(true);
